b2d485042f0693ff9312df686628346f28eae54d
Ezequiel Santos <ezequiel.santos@kxp.consulting>
2023-03-10 10:47:55 -0300

    add: scritps to mysql, storage

--- az-cli/get-vms-cpu-percentage.ps1
+++ az-cli/get-vms-cpu-percentage.ps1
@@ -7,6 +7,46 @@ $startDate = get-date (get-date).AddDays(-30) -Format "yyyy-MM-ddT00:00:00Z"
 
 $vmsMaxCPU = @()
 
+function Get-vmCpuMax {
+    param ($resourceId)     
+
+    $ds = az monitor metrics list --resource $vmId `
+        --metrics "Percentage CPU" `
+        --aggregation maximum `
+        --start-time $startDate `
+        --end-time $endDate `
+        --interval PT1H `
+        --query '{max:value[].timeseries[].data[].maximum}' | ConvertFrom-Json
+
+    $sumPercentage=0
+    foreach ($value in $ds.max) {
+        $sumPercentage+=$value
+    }
+
+    try {
+        $mediaMax=0
+        $mediaMax=($sumPercentage/$ds.max.Count).tostring("##.##")
+        return $mediaMax
+
+    }
+    catch {
+        return 0.00
+    }
+}
+
+foreach ($vmId in $vmIds) {
+    $vmName=$vmId.Split("/")[-1]
+    $cpuMedia = Get-vmCpuMax
+    
+    $vm = [PSCustomObject]@{
+        vmName = "$vmName"
+        "cpuMax %" = [System.Int32]$cpuMedia
+    }
+    $vmsMaxCPU += $vm
+    write-Output "VM: $vmName"
+}
+$vmsMaxCPU | Sort-Object -Property 'cpuMax %' -Descending
+
 foreach ($vmId in $vmIds) {
     $vmName=$vmId.Split("/")[-1]
     $ds = az monitor metrics list --resource $vmId `
@@ -26,7 +66,7 @@ foreach ($vmId in $vmIds) {
         $mediaMax=($sumPercentage/$ds.max.Count).tostring("##.##")
         $vm = [PSCustomObject]@{
             vmName = "$vmName"
-            cpuMax = $mediaMax
+            "cpuMax %" = [System.Int32]$mediaMax
         }
         $vmsMaxCPU += $vm
 
@@ -34,7 +74,7 @@ foreach ($vmId in $vmIds) {
     catch {
         $vm = [PSCustomObject]@{
             vmName = "$vmName"
-            cpuMax = 0.0
+            "cpuMax %" = [System.Int32]0.0
         }
         $vmsMaxCPU += $vm
 
